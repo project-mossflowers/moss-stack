fc35c864b7860829e6ce6f0b186e33677852731c
杨子文 <yangziwen1994@gmail.com>
2025-07-25 14:59:43 +0800

    feat(frontend): 添加openapi客户端生成功能

---
+++ frontend/openapi-ts.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+  input: "./openapi.json",
+  output: {
+    format: 'prettier',
+    lint: 'eslint',
+    path: './src/client',
+  },
+  // exportSchemas: true,
+  plugins: [
+    '@hey-api/schemas',
+    {
+      dates: true,
+      name: '@hey-api/transformers',
+    },
+    {
+      enums: 'javascript',
+      name: '@hey-api/typescript',
+    },
+    {
+      name: '@hey-api/sdk',
+      transformer: true,
+    },
+    '@tanstack/react-query',
+  ],
+})
